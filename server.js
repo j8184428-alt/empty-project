0ea91d9915aae513d1859c305b57e175fdea51f4
j8184428-alt <j8184428@gmail.com>
2026-06-27 18:48:33 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(Server is running on port ${PORT});
+    console.log(`Server is running on port ${PORT}`);
 });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

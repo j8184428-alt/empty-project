4c9a39cbefc4f530a7facc8c1743e47b269ece35
j8184428-alt <j8184428@gmail.com>
2026-06-27 18:10:59 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
+app.listen(PORT, () => {
+    console.log(Server is running on port ${PORT});
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
